e5709148b2a98116fa22478b70168c1cccf391f5
Adam Mashadani <adam.mashadani@elev.ga.ntig.se>
2024-11-11 09:41:16 +0100

    Klar med uppgiften

---
+++ Del2Övningar/Program.cs
@@ -0,0 +1,38 @@
+﻿List<String> names = ["Adam", "Ahmed", "Imad", "Saymmon", "Janan"];
+List<String> leksaker = ["Barbie", "Hund", "Katt", "Svärd", "Pokemon"];
+List<int> värden = [2, 4, 5, 6, 7];
+
+List<string> städer = [];
+
+Random random = new();
+
+
+for (int i = 0; i < names.Count; i++)
+{
+    int randomNum = random.Next(1, 5);
+    Console.WriteLine($" {names[randomNum]} ger {leksaker[randomNum]} betyget {värden[randomNum]}");
+}
+
+string userInput = "";
+while (userInput != "exit")
+{
+    Console.WriteLine("\n" + "Skriv ner namn på en stad");
+    userInput = Console.ReadLine();
+
+    if (userInput.ToLower() == "exit")
+    {
+        break;
+    }
+    else
+    {
+        städer.Add(userInput);
+    }
+}
+
+Console.WriteLine("Städer:");
+for (int i = 0; i < städer.Count; i++)
+{
+    Console.WriteLine(städer[i]);
+}
+
+Console.ReadLine();
